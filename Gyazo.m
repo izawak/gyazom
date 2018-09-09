cfe15e6aaeec86ab349f3ba3f46b5cbe4211525c
Kazuhiko IZAWA <izawa.kazuhiko@gmail.com>
2018-09-09 13:41:11 +0900

    First commit.

---
+++ Gyazo.m
@@ -0,0 +1,72 @@
+BeginPackage["Gyazo`"];
+
+Gyazo::usage = "Gyazo[g_Graphics] or Gyazo[img_Image]";
+GyazoInstall::usage = "Install Gyazo.m";
+
+Options[Gyazo] = {};
+
+Begin["`Private`"];
+
+GyazoInstall::good = "Your token is saved to LocalObject[\"Gyazo.AccessToken\"]";
+GyazoInstall::bad = "Bad token: ``";
+
+GyazoInstall[]:=Block[{token},
+  token = InputString["Input your access token of Gyazo.com"];
+  If[StringQ[token],
+    token = StringReplace[token, "\""->""];];
+  If[StringQ[token] && StringLength[token]==64,
+    Put[token, LocalObject["Gyazo.AccessToken"]];
+    Message[GyazoInstall::good];
+    (* else *),
+    Message[GyazoInstall::bad, token];
+  ];
+];
+
+Gyazo[g_Graphics, opts:OptionsPattern[]]:= Gyazo[Image[g], opts];
+
+ImageFile[img_Image] := Block[{tmpf},
+  tmpf = CreateTemporary[];
+  Export[tmpf, img, "PNG"];
+  tmpf
+  ];
+
+AccessToken[]:=Block[{lo,token},
+  lo = LocalObject["Gyazo.AccessToken"];
+  token = If[FileExistsQ[lo], Get[lo], None];
+  If[StringQ[token]&&StringLength[token]==64,
+    token,
+    None
+  ]
+];
+
+Gyazo::notoken = "Access token is not found. Executing GyazoInstall[]...";
+
+Gyazo[img_Image, opts:OptionsPattern[]] := Block[{imgf, req, token, result},
+    token = AccessToken[];
+    If[Not@StringQ[token],
+      Message[Gyazo::notoken];
+      GyazoInstall[];
+      token = AccessToken[];
+      If[Not@StringQ[token],
+        Return[];
+      ];
+    ];
+    imgf = ImageFile[img];
+    req = HTTPRequest["https://upload.gyazo.com/api/upload", <|
+      "Method" -> "POST",
+      "ContentType" -> "multipart/form-data",
+      "Body" -> <|
+        "access_token" -> token,
+        "imagedata" -> <|"MIMEType" -> "image/png", 
+          "Content" -> File[imgf]|>
+        |>
+      |>
+     ];
+
+    result = URLExecute[req];
+    DeleteFile[imgf];
+    result
+];
+
+End[];
+EndPackage[];
---
+++ README.md
@@ -0,0 +1,17 @@
+Gyazo.m
+=======
+
+A mathematica package to use Gyazo.com
+
+Install
+=======
+
+1. Put Gyazo.m to your Mathematica/Library/Applications directory.
+2. Get an access token from gyazo.com.
+3. Execute GyazoInstall[] and input the access token.
+
+How to use
+==========
+
+Execute Gyazo[img_Image] or Gyazo[g_Graphcis].
+
